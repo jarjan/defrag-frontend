f294f35f062e17b3cd5ac6dd93f16064200df6a3
Merey Zholdas <merey.90@gmail.com>
2019-03-17 01:23:19 +0100

    history improved

--- components/table.js
+++ components/table.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Table } from "antd";
+import { Table, Tag, Badge, Icon } from "antd";
 import axios from "axios";
 
 const columns = [
@@ -8,17 +8,13 @@ const columns = [
     dataIndex: "companyName"
   },
   {
-    title: "Company Tax ID",
+    title: "Signer Tax ID",
     dataIndex: "taxId"
   },
   {
     title: "Beneficial Owner",
     dataIndex: "beneficialOwner"
   }
-  //   {
-  //     title: "Results",
-  //     dataIndex: "results"
-  //   }
 ];
 
 class TableWrapper extends Component {
@@ -44,11 +40,49 @@ class TableWrapper extends Component {
   };
 
   render() {
+    const renderErrors = (errors, row) => {
+      const showErrors = errors.map(error => {
+        return <Tag color="volcano" key={error.message.length}>{error.message}</Tag>
+      });
+      if (showErrors.length === 0) {
+        if (row.isChecked) {
+          return <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" />
+        } else {
+          return <Icon type="check-circle" theme="twoTone" twoToneColor="#cccccc" />
+        }
+      }
+      return showErrors;
+    }
+
+    const renderStatus = (status) => {
+      return status ? <span><Badge status="success" />Yes</span> :
+        <span><Badge status="error" />No</span>
+    }
+
+    const expandedRowRender = (results) => {
+      const columns = [
+        { title: 'Title', dataIndex: 'title', key: 'checkTitle' },
+        { title: 'Is checked', dataIndex: 'isChecked', key: 'isChecked', render: (status) => renderStatus(status) },
+        { title: 'Errors', dataIndex: 'errors', render: (errors, row) => renderErrors(errors, row) }
+      ];
+
+      return (
+        <Table
+          columns={columns}
+          rowKey={item => item.title}
+          dataSource={results}
+          pagination={false}
+          size="small"
+        />
+      );
+    };
+  
     return (
       <Table
         columns={columns}
         rowKey={item => item.taxId}
         dataSource={this.state.data}
+        expandedRowRender={record => expandedRowRender(record.results)}
         loading={this.state.loading}
       />
     );
