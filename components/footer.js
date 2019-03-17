0c8b252f703ff416313b1fb5c88f7e28589c58ec
Zharzhan Kulmyrza <zharzhan@gmail.com>
2019-03-17 10:27:39 +0100

    Footer and logo in components

---
+++ components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Layout } from "antd";
+
+const Footer = () => (
+  <Layout.Footer style={{ textAlign: "center" }}>
+    2019. Big Brother.
+  </Layout.Footer>
+);
+
+export default Footer;
